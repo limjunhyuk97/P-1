79203339f1fe95d74b70c600408a35ea513c964a
jun_lim97 <limjunhyuk97@gmail.com>
2022-01-11 23:26:30 +0900

    [기능구현] 기획서에 있는 Frame2가 팝업하는 기능 구현

---
+++ Project01/public/main.js
@@ -0,0 +1,27 @@
+const containerEl = document.querySelector("#container");
+const toolbarEl = containerEl.querySelector(".toolbar");
+const menukeyEl = toolbarEl.querySelector(".menu-key");
+
+const contentsEl = containerEl.querySelector(".contents");
+const slidemenuEl = contentsEl.querySelector(".slide-menu");
+
+// Frame 1 띄우기
+
+menukeyEl.addEventListener('mouseover', function(){
+  slidemenuEl.classList.add('focused');
+});
+
+menukeyEl.addEventListener('mouseout', function(){
+  if(!slidemenuEl.classList.contains('.clicked'))
+    slidemenuEl.classList.remove('focused');
+});
+
+menukeyEl.addEventListener('click', function(){
+  if(slidemenuEl.classList.contains('.clicked')){
+    slidemenuEl.classList.remove('.clicked');
+    console.log("hi");
+  }
+  else{
+    slidemenuEl.classList.add('.clicked');
+  }
+});
